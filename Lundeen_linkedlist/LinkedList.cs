eddbd0bd7eeafaf27f8e0317388430a2de939090
CopperBanana <aiden.lundeen@student.centralia.edu>
2021-10-02 21:07:36 -0700

    Add project files.

---
+++ Lundeen_linkedlist/LinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lundeen_linkedlist
+{
+    class LinkedList
+    {
+        private Node _head = null;
+        private Node Current;
+
+        public LinkedList() { }
+        public LinkedList(Node H)
+        { _head = H; }
+        
+        public Node getFirst()
+        { // Returns a reference to the first node; if no first, return null
+            return _head;
+        }
+        public void addFirst(Node newNode)
+        {// Add a new node to the front of the list
+            newNode.Next = _head;
+            _head = newNode;
+        }
+        public Node Contains(string match)
+        {// Returns a node with a matching item if no match, return null
+            Current = _head;
+            while (Current != null)
+            {
+                if(Current.Data.Contains(match))
+                { return Current; }
+                Current = Current.Next;
+            }
+            return null;
+        }
+        public void Remove(string match)
+        {// Removes node containing the item and links the two adjacent nodes together
+            Current = _head;
+            if(Current.Data.Contains(match))
+            {
+                if(Current.Next != null)
+                {
+                    _head = Current.Next;
+                    return;
+                } else
+                {
+                    _head = null;
+                    return;
+                }
+            }
+            while (Current.Next != null)
+            {
+                if (Current.Next.Data.Contains(match)) // very long
+                {
+                    if (Current.Next.Next == null)
+                    {
+                        Current.Next = null;
+                    } else
+                    {// should dereference what is originally in current.next, turning it to null would be hard
+                        Current.Next = Current.Next.Next; 
+                    }
+                    return; // in case there are multiple
+                }
+                Current = Current.Next;
+            }
+            Program.Print("No nodes containing " + match + " were found.");
+            Program.Wait();
+            
+        }
+        public void Add(Node newNode)
+        {// Adds a node to the end of the list
+            Current = _head;
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+            Current.Next = newNode; 
+        }
+        public void PrintAllNodes()
+        {// Prints the entire list front to back
+            if(_head == null)
+            {
+                Program.Print("No nodes in list");
+                Program.Wait();
+                return;
+            }
+            Current = _head;
+            int NodeCount = 1;
+
+            while (Current != null)
+            {
+                Program.Print(NodeCount +": " + Current.Data);
+                Current = Current.Next;
+                NodeCount++;
+            }
+            Program.Wait();
+        }
+    }
+}
---
+++ Lundeen_linkedlist/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Lundeen_linkedlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList links = new LinkedList();
+            bool Exit = false;
+            string Choice;
+            char Cases;
+            while (!Exit)
+            {
+                Choice = "";
+                while (Choice == "")
+                {
+                    Choice = Input(
+                    "1. Show Head       \n" +
+                    "2. Add Item        \n" +
+                    "3. Remove Item     \n" +
+                    "4. Search for Item \n" +
+                    "5. Print           \n" +
+                    "6. Exit            \n"
+                    );
+                }
+                Cases = Choice[0];
+                switch (Cases)
+                {
+                    default:
+                        break;
+
+                    case '1': // Show Head
+                        Node First = links.getFirst();
+                        if(First != null)
+                        {
+                            Clear();
+                            Print(First.Data);
+                            Wait();
+                        } else
+                        {
+                            Clear();
+                            Print("Head Node does not exist.");
+                            Wait();
+                        }
+                        break;
+
+                    case '2': // Add Item
+                        string NodeContent = "";
+
+                        while (NodeContent == "")
+                        { NodeContent = Input("Enter Node Contents:"); }
+                        Node newNode = new Node(NodeContent);
+
+                        if (links.getFirst() == null)
+                        { 
+                            links.addFirst(newNode); 
+                        } else
+                        {
+                            links.Add(newNode);
+                        }
+
+                        break;
+
+                    case '3': // Remove Item, no confirmation might need changed
+                        if(links.getFirst() != null)
+                        {
+                            string searchString = "";
+
+                            while (searchString == "")
+                            { searchString = Input("Enter Node data to search: "); }
+                            links.Remove(searchString);
+                            break;
+                        }else
+                        {
+                            Clear();
+                            Print("There are no Nodes.");
+                            Wait();
+                        }
+                        break;
+
+                    case '4': // Search for Item
+                        if (links.getFirst() != null)
+                        {
+                            string search = "";
+                            Node result;
+                            while (search == "")
+                            { search = Input("Enter Node data to search: "); }
+
+                            result = links.Contains(search);
+                            if (result != null)
+                            {
+                                Clear();
+                                Print("Node Contains: " + result.Data);
+                                Wait();
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            Clear();
+                            Print("There are no Nodes.");
+                            Wait();
+                        }
+                        break;
+
+                    case '5': // Print
+                        Clear();
+                        links.PrintAllNodes();
+                        break;
+
+                    case '6': // Exit
+                        Exit = true;
+                        break;
+
+                }
+            }
+                
+        }
+        
+
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static string Input(string helperMessage)
+        { 
+            string input = null;
+
+            Clear();
+            Print(helperMessage);
+            input = Console.ReadLine();
+            
+
+            return input;
+        }
+        public static void Wait()
+        {
+            Console.ReadLine();
+        }
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+
+    }
+}
